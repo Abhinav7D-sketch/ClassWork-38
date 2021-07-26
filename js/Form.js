964465f2311b871021610807a005b6ac1c2242dc
Abhinav7D-sketch <81948494+Abhinav7D-sketch@users.noreply.github.com>
2021-07-26 20:54:23 +0530

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,38 @@
+class Form {
+  constructor() {
+     this.title = createElement("h2");
+     this.input = createInput("name");
+     this.button = createButton("play");
+     this.greeting = createElement("h4");
+  }
+
+  display(){
+    this.title.html("Car Racing Game!!!");
+    this.title.position(130,0);
+  
+    this.input.position(130,160);
+    this.button.position(250,200);
+
+    this.greeting.position(130,100);
+
+    this.button.mousePressed(
+      ()=>{
+        this.input.hide();
+        this.button.hide();
+        player.name = this.input.value();
+        playerCount = playerCount+1;
+        player.index = playerCount;
+        player.update();
+        player.updateCount(playerCount);
+        this.greeting.html("welcome to the gaming world " + player.name)
+        this.greeting.position(130,100);
+      }
+    );
+  }
+
+  hide(){
+    this.button.hide();
+    this.input.hide();
+    this.greeting.hide();
+  }
+}
